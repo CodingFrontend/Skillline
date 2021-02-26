3758e952d750b10ceca6496bafe000271a8548f3
Julia <47003916+MJulia111@users.noreply.github.com>
2021-02-26 13:56:40 +0200

    Update script.js

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -4,11 +4,6 @@ if (isMobile.any()) {
 		let iconMenu = document.querySelector('.icon-menu');
 		let iconMenuClose = document.querySelector('.icon-menu_close');
 
-
-		let menuList = document.querySelector('.menu__list');
-		let menuLinks = menuList.querySelectorAll('.menu__link');
-		let dropdownContents = document.querySelectorAll('.dropdown__content');
-
 		if (iconMenu != null) {
 			let delay = 500;
 			let menuBody = document.querySelector(".menu__body");
@@ -17,7 +12,6 @@ if (isMobile.any()) {
 					body_lock(delay);
 					iconMenu.classList.toggle("active");
 					menuBody.classList.toggle("active");
-					iconMenuClose.classList.toggle("active");
 				}
 			});
 		}
